76dba2c1db34299cc88a99bdae58274793d303f7
Jessica Dao <xjessicadao@gmail.com>
2021-02-24 23:06:33 -0800

    variables added

---
+++ script.js
@@ -0,0 +1,6 @@
+let apiKey = "562517dc15ed5983189073c3d202dae6";
+let currentCity = "#currentCity";
+let tempEl = "#temp";
+let humidityEl = "#humidity";
+let windEl = "#wind";
+let uvIndexEl = "#ultraVI";
