6afb28ba208886567913e46ca53ce5c1bd5c7361
Jessica Dao <xjessicadao@gmail.com>
2021-02-24 23:08:22 -0800

    .

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-let apiKey = "562517dc15ed5983189073c3d202dae6";
-let currentCity = "#currentCity";
-let tempEl = "#temp";
-let humidityEl = "#humidity";
-let windEl = "#wind";
-let uvIndexEl = "#ultraVI";
+var apiKey = "562517dc15ed5983189073c3d202dae6";
+var currentCity = "#currentCity";
+var tempEl = "#temp";
+var humidityEl = "#humidity";
+var windEl = "#wind";
+var uvIndexEl = "#ultraVI";
+
+// City Submit Button
+document
+.querySelector(".search-form")
+.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var cityInput = document.querySelector("#inputValue").value;
+
+  if (document.getElementById("inputValue").value === "") {
+    alert("Please enter a city name!");
+  } else if (cityInput === false) {
+    alert("Please choose a different city!");
+    // return;
+  } else {
+    getWeather(cityInput);
+    saveToStorage(cityInput);
+    renderSaveBtns();
+  }
+});
+
